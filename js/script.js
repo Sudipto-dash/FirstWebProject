f3978838a7d1fec8a2dce6f7526a0acb32f0ca2d
Sudipto Dash <60656465+Sudipto-dash@users.noreply.github.com>
2022-05-22 20:29:02 +0600

    Js Added

---
+++ js/script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded",function () { // Same as document.addEventListener("DOMContentLoaded"...
+
+  // Same as document.querySelector("#navbarToggle").addEventListener("blur",...
+  $("#navbarToggle").blur(function (event) {
+    var screenWidth = window.innerWidth;
+    if (screenWidth < 768) {
+      $("#iconBar").collapse('hide');
+    }
+});
+  });
+  
